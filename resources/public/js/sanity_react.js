88548b548188ba5934ff4ef4a7ccede5d8f1133b
Scott N Fitz <doctorwidget@gmail.com>
2014-09-10 10:38:37 -0700

    final changes to vanilla react tutorial

--- resources/public/js/sanity_react.js
+++ resources/public/js/sanity_react.js
@@ -1,17 +1,70 @@
 /** @jsx React.DOM */
 
-// tutorial1.js
+/** COMPONENT DEFINITIONS 
+The next section defines components but does not instantiate any of them. 
+**/
+
 var CommentBox = React.createClass({
-  render: function() {
-    return (
-     <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.props.data} />
-        <CommentForm />
-      </div>
-    );
-  }
+
+    /** 
+     * loadCommentsFromServer()
+     * A custom method, *not* part of the React Component API
+     */
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    handleCommentSubmit: function(comment) {
+        // TODO: submit to the server to update the database
+        // In the interim, do an "optimistic" local state update
+        var comments = this.state.data;
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+    },
+
+    /** 
+      * getInitialState()
+      * Part of the React Component API
+      */
+    getInitialState: function() {
+        return {
+            data: []
+        };
+    },
+
+   /** 
+     * componentDidMount()
+     * Part of the React Component API
+     */
+    componentDidMount: function() {
+       this.loadCommentsFromServer();
+       setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+
+    /**
+      * render()
+      * Part of the React Component API
+      */
+    render: function() {
+        return (
+                <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+                </div>
+        );
+    }
 });
+
 
 var CommentList = React.createClass({
   render: function() {
@@ -33,11 +86,27 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({author: author, text: text});
+
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+    return;
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
@@ -64,13 +133,23 @@ var Comment = React.createClass({
   }
 });
 
-// special data blob for the tutorial
+/**  INSTANTIATION SECTION 
+
+Now that all of the components are defined up above, it's time to 
+actually create a working UI on the page.
+
+**/
+
+// round 1 data blob for the tutorial
 var tutData = [
   {author: "Pete Hunt", text: "This is one comment"},
   {author: "Jordan Walke", text: "This is *another* comment"}
 ];
+// round 2 replaced that with the static file ``comments.json``
+
+// round 1 just said <CommentBox data={tutData} />
 
 React.renderComponent(
-  <CommentBox data={tutData} />,
+  <CommentBox url="blob/comments.json" pollInterval={10000}  />,
   document.getElementById('content')
 );
